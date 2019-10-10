18e4c0e896d5a8e07f7bd5507383ee79efb5123c
Maria Ninos <marianinos@Marias-MBP.attlocal.net>
2019-10-10 15:04:39 -0700

    Directory restructure

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import python_requires
+from conans import CMake, python_requires
 
 base = python_requires("conanbase/[*]@grifcj/stable")
 
@@ -10,7 +10,19 @@ class LoggerConan(base.get_conanfile()):
         "url": "https://github.com/grifcj/cmake-logger",
         "revision": "auto"
     }
-    build_requires = "gtest/1.8.1@bincrafters/stable"
+    build_requires = (
+            "cmake-modules/[*]@grifcj/stable",
+            "gtest/1.8.1@bincrafters/stable")
+    keep_imports = True
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test()
+
+    def imports(self):
+        self.copy("*.cmake", dst="", src="", folder=True)
 
     def package_info(self):
         self.cpp_info.libs = ["logger"]
